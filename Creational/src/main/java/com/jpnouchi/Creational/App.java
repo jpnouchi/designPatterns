e30dfd62db9a2b865c12d54f4cdf5977cb11015b
unknown <jperalta@jperalta.domebiz.local>
2013-06-05 16:32:32 -0500

    proyectos

---
+++ Creational/src/main/java/com/jpnouchi/Creational/App.java
@@ -0,0 +1,13 @@
+package com.jpnouchi.Creational;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
